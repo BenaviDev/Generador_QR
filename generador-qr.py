363a76c1d83bbeac0cc3a018b272f0c792ce65d9
Santiago Benavidez <sbenavidezr29@gmail.com>
2025-07-12 08:28:59 -0500

    Update generador-qr.py

--- generador-qr.py
+++ generador-qr.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 import qrcode
-# CONFIGURACIÓN (EDITAR ESTO)
-URL_MALICIOSA = "https://url"  # URL
-ARCHIVO_SALIDA = "sigueme.png"    #img
-# Generar QR
-qr = qrcode.make(URL_MALICIOSA)
-qr.save(ARCHIVO_SALIDA)
-print(f"[+] QR generado: {ARCHIVO_SALIDA}")
-print(f"[!] Redirige a: {URL_MALICIOSA}")
-
-
-
-
-
-
-
-#!/usr/bin/env python3
-import qrcode
 #Configuracion /editar/
 url= "https://ejemplo.com" #url
 salida= "imagen.png" # imagen de salida
